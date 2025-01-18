84a181077a06330f43b844f01dafe9e501b02c48
Ashu2310 <ashutoshpande35@gmail.com>
2025-01-19 02:19:02 +0530

    post and delete mappings

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,8 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -19,6 +18,17 @@ public class StudentController {
     @GetMapping("/get")
     public List<Student> getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping("/post")
+    public void addStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+
+    }
+
+    @DeleteMapping("{studentID}")
+    public void deleteStudent(@PathVariable("studentID") Long studentID){
+        studentService.deleteStudentByID(studentID);
     }
 
 
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,22 +1,37 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
+
+    @Autowired private StudentRepository studentRepository;
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Ashutosh",
-                        22,
-                        LocalDate.of(2002, Month.OCTOBER, 23),
-                        "abc.gmail.com"
-                )
-        );
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student){
+
+        Optional<Student> studentEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentEmail.isPresent()){
+            throw new IllegalStateException("email already present");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudentByID(Long studentID) {
+
+        boolean exists = studentRepository.existsById(studentID);
+        if(!exists){
+            throw new IllegalStateException("id not present");
+        }
+        studentRepository.deleteById(studentID);
     }
 }
